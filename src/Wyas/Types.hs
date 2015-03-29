ce0f9a8c20c5ae9353ccbc3affa0f0335c34f021
Scott Clark <scottclark11@gmail.com>
2015-03-29 17:55:45 -0400

    implement 'show' for 'LispVal' types

--- src/Wyas/Types.hs
+++ src/Wyas/Types.hs
@@ -1,9 +1,25 @@
-module Wyas.Types where
+module Wyas.Types (LispVal(..)) where
 
 data LispVal = Atom String
-             | List [LispVal]
+             | Bool Bool
+             | Character Char
              | DottedList [LispVal] LispVal
+             | List [LispVal]
              | Number Integer
              | String String
-             | Character Char
-             | Bool Bool deriving (Show)
+
+instance Show LispVal where
+  show = showVal
+
+showVal :: LispVal -> String
+showVal (Atom val)          = val
+showVal (Bool True)         = "#t"
+showVal (Bool False)        = "#f"
+showVal (Character val)     = show val
+showVal (Number val)        = show val
+showVal (DottedList xs val) = concat ["(", unwordsShowVal xs, " . ", showVal val, ")"]
+showVal (List xs)           = concat ["(", unwordsShowVal xs, ")"]
+showVal (String val)        = concat ["\"", val, "\""]
+
+unwordsShowVal :: [LispVal] -> String
+unwordsShowVal = unwords . map showVal
