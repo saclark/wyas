4d306c6c7cf66d11b873e488557fb6b7a03fde75
Scott Clark <scottclark11@gmail.com>
2015-03-29 00:57:18 -0400

    add some types and parsers

--- src/Wyas/Parser.hs
+++ src/Wyas/Parser.hs
@@ -1,17 +1,47 @@
 module Wyas.Parser where
 
+import Wyas.Types
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+escapableChar :: Parser Char
+escapableChar = oneOf "rnt\\"
+
+escapedChar :: Parser Char
+escapedChar = char '\\' >> escapableChar
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+-- parseString = char '"' >> many (noneOf "\"" <|> escapedChar) >>= \x -> char '"' >> return (String x)
+parseString = do
+                _ <- char '"'
+                x <- many (noneOf "\"" <|> escapedChar)
+                _ <- char '"'
+                return $ String x
+
+parseNumber :: Parser LispVal
+-- parseNumber = many1 digit >>= return . Number . read
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _ -> Atom atom
+
+parseExpr :: Parser LispVal
+parseExpr = parseString <|> parseNumber <|> parseAtom
+
 readExpr :: String -> String
-readExpr input =
-  case result of
-    Left err -> "No match: " ++ show err
-    Right _ -> "Found value"
-  where
-    result = parse (spaces >> symbol) "list" input
+readExpr input = case parse parseExpr "lisp" input of
+                   Left err -> "Syntax error in " ++ show err
+                   Right val -> "Valid: " ++ show val
---
+++ src/Wyas/Types.hs
@@ -0,0 +1,8 @@
+module Wyas.Types where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool deriving (Show)
