33ccad2b53d932330c5ec0bd8d0ff27466b2cbad
Scott Clark <scottclark11@gmail.com>
2015-04-22 23:17:59 -0400

    start adding some error handling

--- src/Wyas/Types.hs
+++ src/Wyas/Types.hs
@@ -1,5 +1,8 @@
 module Wyas.Types (LispVal(..)) where
 
+import Text.ParserCombinators.Parsec (ParseError)
+import Control.Monad.Error
+
 data LispVal = Atom String
              | Bool Bool
              | Character Char
@@ -23,3 +26,34 @@ showVal (String val)        = concat ["\"", val, "\""]
 
 unwordsShowVal :: [LispVal] -> String
 unwordsShowVal = unwords . map showVal
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+instance Show LispError where
+  show = showError
+
+instance Error LispError where
+  noMsg = Default "An error has ocurred"
+  strMsg = Default
+
+showError :: LispError -> String
+showError (UnboundVar message varName) = concat [message, ": ", varName]
+showError (BadSpecialForm message form) = concat [message, ": ", show form]
+showError (NotFunction message func) = concat [message, ": ", func]
+showError (NumArgs expected found) =
+  concat ["Expected: ", show expected, " args; found values ", unwordsShowVal found]
+showError (TypeMismatch expected found) =
+  concat ["Invalid type: expected ", expected, "found, ", show found]
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (Default message) = message
+
+type ThrowsError = Either LispError
+
+-- trapError :: Either LispError String -> Either LispError String
+trapError action = catchError action (return . show)
